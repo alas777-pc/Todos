8c9f19d80c9678c4d1798bc464b0ae8f1b288151
Alexei Lubinets <lubinets01@mail.ru>
2024-09-07 21:46:33 +0500

    change test

--- src/test/java/TasksTest.java
+++ src/test/java/TasksTest.java
@@ -4,16 +4,16 @@ import ru.netology.Epic;
 import ru.netology.Meeting;
 import ru.netology.SimpleTask;
 
-public class TasksTest {
+class TasksTest {
     @Test
-    public void shouldMatchSimpleTask() {
+    void shouldMatchSimpleTask() {
         SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
         Assertions.assertTrue(simpleTask.matches("Позвонить"));
         Assertions.assertFalse(simpleTask.matches("Купить"));
     }
 
     @Test
-    public void shouldMatchEpic() {
+    void shouldMatchEpic() {
         String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
         Epic epic = new Epic(55, subtasks);
         Assertions.assertTrue(epic.matches("Молоко"));
@@ -21,7 +21,7 @@ public class TasksTest {
     }
 
     @Test
-    public void shouldMatchMeeting() {
+    void shouldMatchMeeting() {
         Meeting meeting = new Meeting(
                 555,
                 "Выкатка 3й версии приложения",
