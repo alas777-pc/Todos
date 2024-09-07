598bc2815d3fc8b4571fa1d5656cc5be5f1a32ac
Alexei Lubinets <lubinets01@mail.ru>
2024-09-07 21:53:10 +0500

    Change test 2

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -28,29 +28,6 @@ public class TodosTest {
         Assertions.assertArrayEquals(expected, actual);
     }
 
-    @Test
-    public void shouldSearchTasks() {
-        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
-
-        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
-        Epic epic = new Epic(55, subtasks);
-
-        Meeting meeting = new Meeting(
-                555,
-                "Выкатка 3й версии приложения",
-                "Приложение НетоБанка",
-                "Во вторник после обеда"
-        );
-
-        Todos todos = new Todos();
-        todos.add(simpleTask);
-        todos.add(epic);
-        todos.add(meeting);
-
-        Task[] expected = { simpleTask, meeting };
-        Task[] actual = todos.search("Приложение");
-        Assertions.assertArrayEquals(expected, actual);
-    }
 }
 
 
